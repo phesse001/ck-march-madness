d41d3d642f95285181102615549cb6e88436369d
Patrick Hesse <phesse001@csbsju.edu>
2020-08-09 17:01:56 -0500

    first commit

---
+++ module/test/module.py
@@ -0,0 +1,38 @@
+#
+# Collective Knowledge ()
+#
+# 
+# 
+#
+# Developer: 
+#
+
+cfg={}  # Will be updated by CK (meta description of this module)
+work={} # Will be updated by CK (temporal data)
+ck=None # Will be updated by CK (initialized CK kernel) 
+
+# Local settings
+
+##############################################################################
+# Initialize module
+
+def init(i):
+    """
+
+    Input:  {}
+
+    Output: {
+              return       - return code =  0, if successful
+                                         >  0, if error
+              (error)      - error text if return > 0
+            }
+
+    """
+    return {'return':0}
+
+def run(i)
+    import os
+    cmd = 'sh run.sh'
+    os.system(cmd)
+    
+    return {'return':0}
---
+++ program/simulator/main.cpp
@@ -0,0 +1,20 @@
+#include "Simulator.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+
+
+
+int main(int argc, char *argv[])
+{
+
+  //cout << argv[0] << endl;
+  //cout << argv[1] << endl;
+  //cout << argv[2] << endl;
+  //char *env = getenv("TEST");
+  //cout << env << endl;
+	//int home_field_advantage = std::stoi(argv[1]);
+	//bool apply_scaling = argv[2];
+    run(7,true);
+}
