21fac4ef813e9e0c97f6afc4016cd229119ec96c
Patrick Hesse <phesse001@csbsju.edu>
2020-08-11 10:18:54 -0500

    committing changes before pulling

--- program/simulator/main.cpp
+++ program/simulator/main.cpp
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <iostream>
 #include <fstream>
+#include <string>
 
 
 
@@ -12,9 +13,16 @@ int main(int argc, char *argv[])
   //cout << argv[0] << endl;
   //cout << argv[1] << endl;
   //cout << argv[2] << endl;
-  //char *env = getenv("TEST");
+  char *var1 = getenv("home_field_advantage");
+  char *var2 = getenv("apply_scaling");
+  int home_field_advantage = std::stoi(var1);
+  int apply_scaling = std::stoi(var2);
   //cout << env << endl;
 	//int home_field_advantage = std::stoi(argv[1]);
 	//bool apply_scaling = argv[2];
-    run(7,true);
+	//int home_field_advantage = argv[1];
+	//bool apply_scaling = argv[2];
+	//run(home_field_advantage,apply_scaling);
+
+    run(home_field_advantage,apply_scaling);
 }
